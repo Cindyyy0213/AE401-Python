779a32ef5058cc5c326b7aed47ea9c9d17ed6fd4
Cindyyy0213 <65231460+Cindyyy0213@users.noreply.github.com>
2020-05-19 19:00:42 +0800

    計算圓周

---
+++ spyder lesson 1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 18:57:08 2020
+
+@author: Cindy
+"""
+
+
+radius = input("What's the radius of the circle?")
+print("The circle is")
+print (3.14*int(radius)*int(radius))
+print("type any other numbers and you will find out how big the circle is!")
